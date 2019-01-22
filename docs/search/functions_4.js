2520ec560edbe3a62c441ed867fa6ad0db1a68b3
Patrick Lazik <patrick@yodellabs.com>
2019-01-22 02:42:25 -0500

    Updated to latest framework

---
+++ docs/search/functions_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['sendlocationupdatestoserverwithhosthame_3aport_3a',['sendLocationUpdatesToServerWithHostHame:port:',['../interface_a_l_p_s_location_manager.html#a3f09548933c4f98c5eda2bb1c64e6e0c',1,'ALPSLocationManager']]],
+  ['startupdatinglocation',['startUpdatingLocation',['../interface_a_l_p_s_location_manager.html#a297686ab2968da1cf72a9e525e47ce49',1,'ALPSLocationManager']]],
+  ['stopsendinglocationudpatestoserver',['stopSendingLocationUdpatesToServer',['../interface_a_l_p_s_location_manager.html#a7c047e1d592849c79e1c6a1862f37a88',1,'ALPSLocationManager']]],
+  ['stopupdatinglocation',['stopUpdatingLocation',['../interface_a_l_p_s_location_manager.html#a05d6a53c0148c04b5a591772be6c36a5',1,'ALPSLocationManager']]]
+];
---
+++ docs/struct_a_l_p_s_location3_d.js
@@ -0,0 +1,6 @@
+var struct_a_l_p_s_location3_d =
+[
+    [ "x", "struct_a_l_p_s_location3_d.html#a91066c070d862a61f5949e96a98a67ba", null ],
+    [ "y", "struct_a_l_p_s_location3_d.html#ac57aed0ba567aabe2915b3486189aefd", null ],
+    [ "z", "struct_a_l_p_s_location3_d.html#a9345825fcd2a333d8456ea6a076f34c2", null ]
+];
